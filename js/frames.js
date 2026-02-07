cab1c97ae3520eb5870401437604e65999344d66
nicol <nicolasal.aguilarm@gmail.com>
2026-02-07 17:48:18 -0300

    Update site

--- js/frames.js
+++ js/frames.js
@@ -1,46 +1,58 @@
    
 function frames(variable){
-    document.getElementById("enframe").src="pages/home.html";
-    if (variable==1){
-        document.getElementById("enframe").src="pages/home.html";
+    // Default: Home (si llega un número que no existe)
+    document.getElementById("enframe").src = "pages/home.html";
+
+    // 1) About me
+    if (variable == 5){
+        document.getElementById("enframe").src = "pages/about_me.html";
     }
-    else if (variable==2){
-        document.getElementById("enframe").src="pages/submitted.html";
+
+    // 2) Writings
+    else if (variable == 15){
+        document.getElementById("enframe").src = "pages/EssaysIndex.html";
     }
-    else if (variable==3){
-        document.getElementById("enframe").src="pages/published.html";
+    else if (variable == 16){
+        document.getElementById("enframe").src = "pages/writingsonmusic.html";
     }
-    else if (variable==4){
-        document.getElementById("enframe").src="pages/my_thesis.html";
+    else if (variable == 17){
+        document.getElementById("enframe").src = "pages/Poetry.html";
     }
-    else if (variable==5){
-        document.getElementById("enframe").src="pages/about_me.html";
+    else if (variable == 18){
+        document.getElementById("enframe").src = "pages/others.html";
     }
-    else if (variable==6){
-        document.getElementById("enframe").src="pages/2019.html";
-    }   
-    else if (variable==7){
-        document.getElementById("enframe").src="pages/2018.html";
-    }     
-    else if (variable==8){
-        document.getElementById("enframe").src="pages/2017.html";
-    }  
-    else if (variable==9){
-        document.getElementById("enframe").src="pages/2016.html";
-    }  
-    else if (variable==10){
-        document.getElementById("enframe").src="pages/2020.html";
-    }  
-    else if (variable==11){
-        document.getElementById("enframe").src="pages/2021.html";
-    } 
-    else if (variable==12){
-        document.getElementById("enframe").src="pages/2022.html";
-    }    
-    else if (variable==13){
-        document.getElementById("enframe").src="pages/2023.html";
-    }  
-    else if (variable==14){
-        document.getElementById("enframe").src="pages/2024.html";
-    }  
-} 
+
+    // 3) Music
+    else if (variable == 19){
+        document.getElementById("enframe").src = "pages/reviews.html";
+    }
+    else if (variable == 20){
+        document.getElementById("enframe").src = "pages/Interviews.html";
+    }
+    else if (variable == 21){
+        document.getElementById("enframe").src = "pages/thelist.html";
+    }
+
+    // 4) Miscellaneous
+    else if (variable == 22){
+        document.getElementById("enframe").src = "pages/misc_wikipedia.html";
+    }
+    else if (variable == 23){
+        document.getElementById("enframe").src = "pages/misc_oracle.html";
+    }
+    else if (variable == 24){
+        document.getElementById("enframe").src = "pages/misc_favorite_websites.html";
+    }
+    else if (variable == 25){
+        document.getElementById("enframe").src = "pages/misc_favorite_yt.html";
+    }
+    else if (variable == 26){
+        document.getElementById("enframe").src = "pages/misc_poetry_anthology.html";
+    }
+
+    // 5) Blog
+    else if (variable == 27){
+        document.getElementById("enframe").src = "pages/blog.html";
+    }
+}
+
